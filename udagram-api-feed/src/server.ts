77c83102895de443a1a80ede661aca8c431935af
Lucas Silvestre Livero-LSIL <lsil@motorola.com>
2021-01-26 14:05:20 -0400

    project files

---
+++ udagram-api-feed/src/server.ts
@@ -0,0 +1,41 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+import { IndexRouter } from './controllers/v0/index.router';
+import { V0_FEED_MODELS } from './controllers/v0/model.index';
+import { sequelize } from './sequelize';
+
+
+(async () => {
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running in port: ${port}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
